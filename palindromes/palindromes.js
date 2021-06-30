934953286d90ea48fdb2d18520dbf6640a35c0d4
Garrett Anderson <keaton85@runbox.com>
2021-06-29 21:26:10 -0500

    solved palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    
+        var str1 = str.toLowerCase().replace(/[^A-Za-z0-9]/g, '');
+        
+        var str2 = Array.from(str1).reverse().toString().replace(/\W/g, '');
+      
+        if (str1 === str2) return true;
+        
+        else return false;        
+      
 };
 
 module.exports = palindromes;
