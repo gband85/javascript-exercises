a65b24537c100fa6e301d8c058a1a1ca555cac70
Garrett Anderson <keaton85@runbox.com>
2021-06-29 21:15:47 -0500

    solved calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce(function(a,b) {
+    return a+b;
+  },0)
 };
 
-const multiply = function() {
+const multiply = function(array) {
+if (array.length) {
+  return array.reduce(function(a,b) {
+    return a*b;
+  })
 
+}
+else  {
+  return 0;
+}
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n===0) return 1;
+	return n*factorial(n-1);
 };
 
 module.exports = {
