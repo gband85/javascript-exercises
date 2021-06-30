3840e906d69a58d24f453fd370d7b1d93d8f206b
Garrett Anderson <keaton85@runbox.com>
2021-06-29 21:27:02 -0500

    solved leapyear

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,9 @@
 const leapYears = function() {
-
+    if (year % 4==0 && year % 100 != 0) {
+        return true;
+    }
+    else if (year % 4==0 && year % 100==0 && year % 400==0) { return true; }
+    else { return false };
 };
 
 module.exports = leapYears;
