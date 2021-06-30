104d1b1b0125c16f8f9be52073427ff4fe4b6aaf
Garrett Anderson <keaton85@runbox.com>
2021-06-30 13:37:41 -0500

    solved helloWorld, removeFromArray, repeatString, reverseString

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(...args) {
+    let array=args[0];
+   // console.log(args);
+    for (let i=1;i<args.length;i++) {
+            
+            if (array.indexOf(args[i])===-1) continue;
+    array.splice(array.indexOf(args[i]),1);
+        }
+        return array;
+    }
 
 module.exports = removeFromArray;
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
-};
+const reverseString = function(string) {
+    let stringArray=string.split('');
+    stringArray.reverse();
+    let reversedString='';
+    for (let i=0; i<stringArray.length; i++) {
+    reversedString=reversedString.concat(stringArray[i])
+    }
+    return reversedString;
+    };
 
 module.exports = reverseString;
