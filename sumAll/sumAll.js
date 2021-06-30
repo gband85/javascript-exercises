b293d5f46de85bd63b552084999b59c40685134a
Garrett Anderson <keaton85@runbox.com>
2021-06-30 15:49:17 -0500

    solved sumAll,tempConversion

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
-};
+const sumAll = function(startNum,endNum) {
+    let sum=0;
+    let tempSwap=0;
+    if (startNum<0 || endNum<0 || typeof(startNum)!=="number" || typeof(endNum)!=="number") { return "ERROR";}
+    if (endNum<startNum) {
+        tempSwap=endNum;
+        endNum=startNum;
+        startNum=tempSwap;
+    }
+    for (let i=startNum; i<=endNum; i++) {
+        sum+=i;
+    }
+    return sum;
+    };
 
 module.exports = sumAll;
--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,12 @@
-const ftoc = function() {
-
-};
-
-const ctof = function() {
-
-};
-
-module.exports = {
-  ftoc,
-  ctof
-};
+const ftoc = function(tempFahrenheight) {
+  return parseFloat(((5/9) * (tempFahrenheight -32)).toFixed(1));
+  };
+  
+  const ctof = function(tempCelsius) {
+  return parseFloat(((9/5)*tempCelsius+ 32).toFixed(1));
+  };
+  
+  module.exports = {
+    ftoc,
+    ctof
+  };
